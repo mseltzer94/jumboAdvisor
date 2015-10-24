db9c205a18e7ed0a21e36738c9b0bc33340fb70c
jclaw <jclaw3456@gmail.com>
2015-10-24 01:04:46 -0400

    halfway finished dropping

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,7 +32,7 @@ angular.module('xmpl.directive', []);
 
 angular.module('xmpl.filter', []);
 
-angular.module('xmpl', ['xmpl.service', 'xmpl.directive', 'xmpl.filter', 'viz'])
+angular.module('xmpl', ['demo', 'xmpl.service', 'xmpl.directive', 'xmpl.filter', 'viz'])
 
     .run(function(greeter, user) {
     // This is effectively part of the main method initialization code
@@ -44,6 +44,29 @@ angular.module('xmpl', ['xmpl.service', 'xmpl.directive', 'xmpl.filter', 'viz'])
 
     .controller('XmplController', function($scope, greeter, user){
     	$scope.greeting = greeter.greet(user.name);
+        $scope.major = "HELLLLO";
     });
 
 })(window.angular);
+
+
+angular.module("demo", ['dndLists', 'ngAnimate'])
+.controller("SimpleDemoController", function($scope) {
+
+    $scope.models = {
+        selected: null,
+        lists: {"A": [], "B": []}
+    };
+
+    // Generate initial model
+    for (var i = 1; i <= 3; ++i) {
+        $scope.models.lists.A.push({label: "Item A" + i});
+        $scope.models.lists.B.push({label: "Item B" + i});
+    }
+
+    // Model to JSON for demo purpose
+    $scope.$watch('models', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+    }, true);
+
+});
