76cc68348f899c121d23dc2d14b1bb24673ac276
Ben Tanen <ben.tanen@gmail.com>
2015-10-23 22:58:15 -0400

    added backend / cleaned up json

---
+++ backend/server.js
@@ -0,0 +1,142 @@
+// Express initialization
+var express = require('express');
+var bodyParser = require('body-parser');
+var validator = require('validator'); // See documentation at https://github.com/chriso/validator.js
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mongo initialization, setting up a connection to a MongoDB  (on Heroku or localhost)
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/jumboAdvisor';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
+	console.log('initial connection made');
+});
+
+/*
+app.post('/sendLocation', function (request, response) {
+	// enable Cross-Origin Resource Sharing
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+	
+	// send back JSON
+	response.set('Content-Type', 'application/json');
+
+	// get parameters from request
+	var login = request.body.login;
+	var lat = request.body.lat;
+	var lng = request.body.lng;
+	var time = new Date().getTime();
+
+	// if valid parameters (login,lat,lng)
+	if (login != undefined && lat != undefined && lng != undefined) {
+		loc = db.collection('locations', function (err, collection) {
+			// entry to insert
+			var entry = {'login': login, 'lat': parseFloat(lat), 'lng': parseFloat(lng), 'created_at': time};
+
+		  	var id = collection.insert(entry, function(err, saved) {
+				if (!err) {
+					collection.find().toArray(function(er, cursor) {
+						if (!err) {
+							var return_array = {'characters': [], 'students':[]};
+
+							// push last 100 locations
+							for (var count = 0; count < 100; count++) {
+								// make sure within bounds of DB data
+								if (count < cursor.length) {
+									return_array['students'].push(cursor[count]);
+								}
+							}
+
+							// sort array based on time (see readme for algorithm source)
+							return_array['students'].sort(function(a,b) {
+								var x = a['created_at'];
+								var y = b['created_at'];
+								return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+							});
+
+							// send array
+							response.send(JSON.stringify(return_array));
+						}
+					});
+				}
+				else {
+					response.send({"status":"Error: data did not go through"});
+				}
+			});
+		});
+	// else, send error message (message from ChickenOfTheSea)
+	} else {
+		response.send({"status":"Error: data did not go through"});
+	}
+});
+*/
+
+/*
+app.get('/getCurrently', function (request, response) {
+	// enable Cross-Origin Resource Sharing
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "X -Requested-With");
+
+	// send back JSON
+	response.set('Content-Type', 'application/json');
+
+	var password = request.query.password;
+	var category = request.query.category;
+	var return_array = [ ];
+
+	// if user included login
+	if (password === 'foodtape') {
+		// if (['listening', 'reading', 'watching', 'working'].indexOf(category) >= 0) {
+		res = db.collection('currently', function (err, collection) {
+			collection.find().toArray(function(er, cursor) {
+				if (!err) {
+					for (i = 0; i < cursor.length; i++) {
+						if (cursor[i][''])
+					}
+				}
+			});
+		});
+		} else {
+			console.log('invalid category');
+			response.send([]);
+		}
+		
+
+	// no login specified, send blank array
+	} else {
+		response.send([]);
+	}
+});
+*/
+
+app.get('/', function (request, response) {
+	// enable cross-orign resource sharing
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+	// send back JSON
+	response.set('Content-Type', 'application/json');
+
+	var degree = request.query.degree;
+
+	if (degree != null) {
+		degrees = db.collection('degrees', function (err, collection) {
+			collection.find().toArray(function(er, cursor) {
+				if (!err) {
+					for (i = 0; i < cursor.length; i++) {
+						console.log('hey');
+					}
+				}
+			});
+		});
+
+		response.send(JSON.stringify({'degree': degree}));
+	} else {
+		response.send([])
+	}
+
+});
+
+app.listen(process.env.PORT || 3000);
